54c45c4d3bf81234736a1288955fc5137e87dc52
Zeeshan <sk.zeeshan1992@gmail.com>
2021-04-10 16:08:43 -0400

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
+import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import React from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+})
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+interface Props {}
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
